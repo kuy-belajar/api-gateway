720971dd210977a4d298d53c5c42922d55f29dc4
husfuu <muhammadhusninurfadillah@gmail.com>
2022-08-06 04:05:14 +0800

    SAIN-26 | media service integration

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const coursesRouter = require("./routes/courses");
-const mediaRouter = require("./routes/media");
-const ordersRouter = require("./routes/orders");
-const paymentsRouter = require("./routes/payments");
+const indexRouter = require("./routes/index.routes");
+const usersRouter = require("./routes/users.routes");
+const coursesRouter = require("./routes/courses.routes");
+const mediaRouter = require("./routes/media.routes");
+const ordersRouter = require("./routes/orders.routes");
+const paymentsRouter = require("./routes/payments.routes");
 
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
--- controllers/apiAdapter.js
+++ controllers/apiAdapter.js
@@ -1,9 +1,10 @@
 const axios = require("axios");
+
 const { TIMEOUT } = process.env;
 
 module.exports = (baseUrl) => {
   return axios.create({
     baseURL: baseUrl,
-    timeout: TIMEOUT
+    timeout: parseInt(TIMEOUT)
   });
 };
